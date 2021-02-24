61e751a641cce2033807581785a6dc92f7db0ff2
PauloSGM <meiotodo@hotmail.com>
2021-02-24 17:38:05 -0300

    ajustes

--- js/calc.js
+++ js/calc.js
@@ -5,14 +5,14 @@
             const b = document.getElementById('altura');
 
             function mudaCor(){
-                document.getElementById('formTeste:texto1').style.fontcolor = '#ed0909';	
+                document.getElementById(res).style.fontcolor = '#ed0909';	
                  }
 
             calcularBtn.addEventListener("click", function () {
                 let res = a.value / (b.value * b.value);
                 console.log(res);
-                var arredondando = parseFloat(res.toFixed(0));
-                if (res > 30) { document.getElementById("totalAlto").innerHTML = arredondando + " Seu resultado deu acima do permitido. Procure acompanhamento médico"; }
-                else document.getElementById("totalOk").innerHTML = arredondando + "Parabéns! Seu resultado está dentro do limite aceitável. Continue cuidando de seu peso";
+                let arredondando = parseFloat(res.toFixed(0));
+                if (res > 30) { document.getElementById("totalAlto").innerHTML = arredondando + " Foi o resultado do seu IMC. Seu resultado deu acima do permitido. Procure acompanhamento médico."; }
+                else document.getElementById("totalOk").innerHTML = arredondando + " Foi o resultado do seu IMC. Parabéns! Seu resultado está dentro do limite aceitável. Continue cuidando de seu peso.";
                     ;
             });
