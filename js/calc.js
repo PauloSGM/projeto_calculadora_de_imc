6c6d63883a7a9ec6963e40d07697567368611f38
PauloSGM <meiotodo@hotmail.com>
2021-02-28 10:54:55 -0300

    condicionais ajustadas

--- js/calc.js
+++ js/calc.js
@@ -5,17 +5,25 @@ const a = document.getElementById('peso');
 const b = document.getElementById('altura');
 
 calcularBtn.addEventListener("click", function () {
-    document.getElementById("totalAlto").innerHTML = "";
-    document.getElementById("totalBaixo").innerHTML = "";
-    document.getElementById("totalOk").innerHTML = "";
+    document.getElementById("totalMagro").innerHTML = "";
+    document.getElementById("totalNormal").innerHTML = "";
+    document.getElementById("totalSobre").innerHTML = "";
+    document.getElementById("totalObeso").innerHTML = "";
     let res = a.value / (b.value * b.value);
     console.log(res);
     let arredondando = parseFloat(res.toFixed(1));
-    if (res > 30) {
-        document.getElementById("totalAlto").innerHTML = arredondando + " é o resultado do seu IMC. Seu resultado deu acima do permitido. Procure acompanhamento médico.";
+    if (res <18) {
+        document.getElementById("totalMagro").innerHTML = arredondando + " é o seu resultado. Tenha cuidado. Seu IMC está abaixo do limite aceitável. Procure um especialista.";
     }
-    else if (res < 18) {
-        document.getElementById("totalBaixo").innerHTML = arredondando + " é o resultado do seu IMC. Tenha cuidado. Seu resultado está abaixo do limite aceitável. Procure um especialista.";
+    else if (res <25) {
+        document.getElementById("totalNormal").innerHTML = arredondando + " é o seu resultado. Parabéns! Seu IMC está dentro do ideal. Continue cuidando de sua alimentação.";
     }
-    else document.getElementById("totalOk").innerHTML = arredondando + " é o resultado do seu IMC. Parabéns! Seu resultado está dentro do limite aceitável. Continue cuidando de seu peso.";
-});
+    else if (res <30) {
+        document.getElementById("totalSobre").innerHTML = arredondando + " é o seu resultado. Tenha cuidado. Seu está IMC um pouco acima do limite aceitável. Procure um especialista.";
+    }
+    else if (res <31) {
+        document.getElementById("totalSobre").innerHTML = arredondando + " é o seu resultado. Procure a ajuda de um especialista! Seu IMC está <Strong>muito</strong> acima do limite aceitável.";
+    }
+    else alert("Procure ajuda médica para cuidar de sua alimentação. Você está muito acima do peso ideal.")
+     document.getElementById("totalObeso").innerHTML = arredondando + " é o seu resultado. Procure a ajuda de um especialista! Seu IMC está <Strong>muito</strong> acima do limite aceitável.";
+}); 
